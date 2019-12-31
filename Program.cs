2fd1c95f1758856b45b29811f293dfb1ed338dc9
rodri <rmzancs@gmail.com>
2019-12-31 15:37:53 -0300

    Add New Archives

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotNet_olaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("The Current Time is: " + DateTime.Now);
+        }
+    }
+}
